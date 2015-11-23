62bb146112809b8ee1133693ed6e5b61d35a8b05
Sean Massih <seanfromiran@gmail.com>
2015-11-23 07:05:10 -0800

    Update 5.4 solution.rb

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -21,9 +21,12 @@ end
 
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+=begin
+  -I used EACH, as a one-stop shop to iterate through both the Array and the Hash.
+  - I used the SHOVEL method to get values that met the boolean into the output array.
+  - I used is_a? method in the first portion of the problem because the include? method (described below) won't work on an integer. I put the is_a? method at the first part of an && boolean so the program would bypass the second statement if the first isn't true.
+  -Finally I used a include? method to determine if the value INCLUDED the letter I was looking for
+=end
 
 # Person 2
 def my_array_modification_method!(source, thing_to_modify)
@@ -44,10 +47,13 @@ end
 
 #puts my_hash_modification_method!(my_family_pets_ages, 2)
 
-# Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+=begin
+Identify and describe the Ruby method(s) you implemented.
+For my array method, I used an each iterator as well as the is_a? method to identify which elements of my array were integers. The hash method uses an each iterator which allows us to increment all the values of our hash.
+=end
+
+
+
 
 
 # Person 3
@@ -66,10 +72,21 @@ def my_hash_sorting_method(source)
 end
 
 
-# Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+=begin
+Identify and describe the Ruby method(s) you implemented.
+- grep: Calling grep on 'source' with the 'String' argument returns an array of the strings in the 'source' array.
+It excludes anything in 'source' that isn't a string (in this case, it excludes the integers). The same idea applies
+on the next line, where I call grep on 'source' to return an array of the Fixnums in 'source'.
+- unshift: Calling unshift on 'sorted_strings' with the argument 'sorted_ints' inserts the array
+of sorted integers ('sorted_ints') at the beginning of the 'sorted_strings' array.
+- flatten!: Calling flatten! merges the 'sorted_ints' array with the 'sorted_strings' array so that the
+whole thing is a one-dimensional array. I used the destructive flatten! method to do the operation in place,
+but it's probably not necessary since the block on line 45 is returned and assigned to a single variable.
+- sort: The sort method compares elements one after another and the block tells the method how to sort the elements.
+- to_a: I called to_a on the method's argument to convert the input from a hash to an array of 2-element arrays.
+This is necessary because the method that I use next takes arrays and not hashes.
+- sort_by!: I called sort_by! to sort the 2-element arrays by the value of the second element.
+=end
 
 
 # Person 4
