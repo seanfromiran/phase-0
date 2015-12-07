a7bde307bf041b4997249a2d6129bc3b661fa0d8
Sean Massih <seanfromiran@gmail.com>
2015-12-07 09:21:19 -0800

    Add 7.7 file

--- week-7/game.js
+++ week-7/game.js
@@ -48,30 +48,72 @@ function knight_attack() {
 }
 
 while (dragon.health > 0 && knight.health > 0) {
-  	knight_attack();
+ 	knight_attack();
 	dragon_attack();
 	if (dragon.health <= 0) {
 		console.log("You have defeated the Dragon!");
 	}
   	if (knight.health <= 0) {
-		console.log("You're roadkill buddy!")
+		console.log("You're roadkill buddy!");
 	}
 }
 
 
 // Refactored Code
 
+knight = {
+	damage: 40,
+	health: 400
+}
 
+dragon = {
+	damage: 60,
+	health: 400
+}
 
+function dragon_attack() {
+	if((Math.random()*100)<42) {
+		knight.health -= dragon.damage;
+	}
+}
 
+function knight_attack() {
+	dragon.health -= knight.damage;
+}
 
+while (dragon.health > 0 && knight.health > 0) {
+ 	knight_attack();
+	dragon_attack();
+	if (dragon.health <= 0) {console.log("You have defeated the Dragon!");}
+  if (knight.health <= 0) {console.log("You're roadkill buddy!");}
+}
 
 // Reflection
-//
-//
-//
-//
-//
-//
-//
-//
+
+/*  What was the most difficult part of this challenge?
+
+		Honestly, it was just dealing with JavaScript in general! Still being 
+		pretty new to the language and having to deal with all the new syntax 
+		compared to Ruby has been a pain. 
+
+    What did you learn about creating objects and functions that interact with 
+    one another?
+
+		I felt like I was just working with hashes and calling methods on these 
+		hashes. Maybe I'm coming from too much of a Ruby background, but after 
+		doing previous challenges I feel like I had a really good understanding of 
+		how objects and functions interact from the get go. 
+
+    Did you learn about any new built-in methods you could use in your 
+    refactored solution? If so, what were they and how do they work?
+
+		I didn't have time to add additional functionality to my game so I didn't
+		incorporate any new built-in methods.
+
+    How can you access and manipulate properties of objects?
+
+		obj.prop_name is a good way to access a certain property of an object. As 
+		far as manipulating goes, we can say obj.prop_name = prop_value to change 
+		things up.
+
+*/
